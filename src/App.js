3494e84a2109d879dd0b8065581ab551c34f6c7e
sohail20 <bhattisohail20@gmail.com>
2021-07-08 05:41:28 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import PriceCard from './Components/Card';
+import NavBar from './Components/NavBar';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
+  const [Data, setData] = useState({
+    low: 0,
+    high: 0,
+    last: 0,
+  });
+  const [Loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      axios.get("https://nitinr-cors.herokuapp.com/https://api.wazirx.com/api/v2/tickers/dogeusdt").then((res)=>{
+        setData(res.data.ticker)
+        setLoading(false)
+      })
+      .catch((err)=>{
+        setLoading(false)
+      })
+    },1000)
+  },[Data])
+  return Loading?<p>Loading...</p>:(
+    <div >
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="prices-container">
+          <PriceCard type="high" price={Data.high}/>
+          <PriceCard type="low" price={Data.low}/>
+          <PriceCard type="current" price={Data.last}/>
+        </div>
       </header>
     </div>
   );
